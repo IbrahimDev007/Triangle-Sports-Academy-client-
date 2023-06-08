50aa0b4fe26961a3b86f420635cfd80c7c89087f
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-06-08 21:06:46 +0600

    simple folder structure route setup and query provider and  react helmet setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import router from "./Routes/Routes";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+	<HelmetProvider>
+		<QueryClientProvider client={queryClient}>
+			<div className="max-w-screen-xl mx-auto">
+				<RouterProvider router={router} />
+			</div>
+		</QueryClientProvider>
+	</HelmetProvider>
 );
