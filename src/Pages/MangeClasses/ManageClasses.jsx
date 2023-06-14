c5fd935b181d679c7f4bd4684d559cf8a4308651
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-06-14 13:09:25 +0600

    mahage classs layout done

--- src/Pages/MangeClasses/ManageClasses.jsx
+++ src/Pages/MangeClasses/ManageClasses.jsx
@@ -1,103 +1,75 @@
 import { Helmet } from "react-helmet-async";
 // import Swal from "sweetalert2";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const ManageClasses = () => {
-	return <div>manage clasees</div>;
+	return (
+		<div>
+			{" "}
+			<div className="w-full">
+				<Helmet>
+					<title>triangle Sports Academe |Manage class</title>
+				</Helmet>
+
+				<div className="overflow-x-auto w-full">
+					<table className="table w-full">
+						{/* head */}
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Class</th>
+								<th>Class Name</th>
+								<th>Instructor</th>
+								<th>Email</th>
+								<th>Seats</th>
+								<th>Price</th>
+								<th>staatus</th>
+								<th>class update</th>
+							</tr>
+						</thead>
+						<tbody>
+							{/* {classes.map((class, index) => ( */}
+							{/* <tr key={class._id}> */}
+							<tr>
+								<td>{1}</td>
+								<td>
+									<div className="avatar">
+										<div className="mask mask-squircle w-12 h-12">
+											<img
+												// src={class.image}
+												src={""}
+												alt="Avatar Tailwind CSS Component"
+											/>
+										</div>
+									</div>
+								</td>
+								<td>class.name</td>
+								{/* <td className="text-end">${class.price}</td> */}
+								<td className="text-end">class.instractor</td>
+								<td className="text-end">class.seats</td>
+								<td className="text-end">class.price</td>
+								<td className="text-end">class.status</td>
+
+								<td>
+									<td className="text-end btn-ghost" onClick={""}>
+										Approved
+									</td>
+									<td className="text-end btn-ghost" onClick={""}>
+										Denay
+									</td>
+									<td className="text-end btn-ghost" onClick={""}>
+										Feedback
+									</td>
+								</td>
+							</tr>
+							{/* )) */}
+							{/* } */}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ManageClasses;
-{
-	const SelectClasses = () => {
-		//   const [selected, refetch] = useSelected();
-		// console.log(selected);
-		// const total = selected.reduce((sum, studentClass) => studentClass.price + sum, 0);
-
-		const handleDelete = (studentClass) => {
-			// Swal.fire({
-			// 	title: "Are you sure?",
-			// 	text: "You won't be able to revert this!",
-			// 	icon: "warning",
-			// 	showCancelButton: true,
-			// 	confirmButtonColor: "#3085d6",
-			// 	cancelButtonColor: "#d33",
-			// 	confirmButtonText: "Yes, delete it!",
-			// }).then((result) => {
-			// 	if (result.isConfirmed) {
-			// 		fetch(`https://localhost:3000/selecteds/${studentClass._id}`, {
-			// 			method: "DELETE",
-			// 		})
-			// 			.then((res) => res.json())
-			// 			.then((data) => {
-			// 				if (data.deletedCount > 0) {
-			// 					refetch();
-			// 					Swal.fire("Deleted!", "Your file has been deleted.", "success");
-			// 				}
-			// 			});
-			// 	}
-			// });
-		};
-		return (
-			<div>
-				{" "}
-				<div className="w-full">
-					<Helmet>
-						<title>triangle Sports Academe | My selected</title>
-					</Helmet>
-					<div className="uppercase font-semibold h-[60px] flex justify-evenly items-center">
-						<h3 className="text-3xl">Total classes: selected.length</h3>
-						<h3 className="text-3xl">Total Price: total</h3>
-						<Link to="/dashboard/payment">
-							<button className="btn btn-warning btn-sm">PAY</button>
-						</Link>
-					</div>
-					<div className="overflow-x-auto w-full">
-						<table className="table w-full">
-							{/* head */}
-							<thead>
-								<tr>
-									<th>#</th>
-									<th>class</th>
-									<th>class Name</th>
-									<th>Price</th>
-									<th>Action</th>
-								</tr>
-							</thead>
-							<tbody>
-								{/* {selected.map((studentClass, index) => ( */}
-								{/* <tr key={studentClass._id}> */}
-								<tr>
-									<td>{1}</td>
-									<td>
-										<div className="avatar">
-											<div className="mask mask-squircle w-12 h-12">
-												<img
-													// src={studentClass.image}
-													src={""}
-													alt="Avatar Tailwind CSS Component"
-												/>
-											</div>
-										</div>
-									</td>
-									<td>studentClass.name</td>
-									{/* <td className="text-end">${studentClass.price}</td> */}
-									<td className="text-end">$222</td>
-									<td>
-										<button
-											onClick={() => handleDelete(studentClass)}
-											className="btn btn-ghost bg-red-600  text-white"
-										>
-											Delete
-										</button>
-									</td>
-								</tr>
-								{/* )) */}
-								{/* } */}
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-		);
-	};
-}
